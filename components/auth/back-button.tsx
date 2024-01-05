9cf92037d2d95061b01cd6617226a0f3983c9553
Kelvin Bush <kelybush@gmail.com>
2024-01-05 13:30:04 +0300

    add shadcn form

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export const BackButton = ({ label, href }) => {
+  return (
+    <Button variant={'link'} className={'font-normal w-full'} size={'sm'} asChild>
+      <Link href={href}>{label}</Link>
+    </Button>
+  )
+}
