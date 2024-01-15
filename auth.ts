622cd13a96b34efc05b1b46aada75d9d5e5be063
Kelvin Bush <kelybush@gmail.com>
2024-01-15 10:07:56 +0300

    set up role

--- auth.ts
+++ auth.ts
@@ -9,6 +9,15 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    async jwt({ token }) {
+      return token
+    },
+    async session({ session, token }) {
+      if (token.sub && session.user) session.user.id = token.sub
+      return session
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig
